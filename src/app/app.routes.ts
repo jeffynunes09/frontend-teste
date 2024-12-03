ab641d09f8def59d701957862b42326b41cc0c07
jeffynunes09 <nunesjefferson425@gmail.com>
2024-12-02 21:14:17 -0300

    criação pagina home da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthorListComponent } from './author-list/author-list.component';  // Componente correto importado
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+// Definição das rotas
+export const routes: Routes = [
+  {
+    path: "", component:HomeComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],  // Configuração de rotas com RouterModule.forRoot()
+  exports: [RouterModule]  // Exportação do RouterModule
+})
+export class AppRoutingModule { }
