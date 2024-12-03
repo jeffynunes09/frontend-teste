c4e29cf11ed09c7dc527072d376b97bf8a23f7b9
jeffynunes09 <nunesjefferson425@gmail.com>
2024-12-02 21:39:24 -0300

    criação da da paginação e das rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,16 +2,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthorListComponent } from './author-list/author-list.component';  // Componente correto importado
 import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 // Definição das rotas
 export const routes: Routes = [
   {
-    path: "", component:HomeComponent
+    path: "", component:HomeComponent,
+    
+  },
+  {
+    path:"books", component:BookListComponent
+  },
+  {
+    path:"authors", component:AuthorListComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],  // Configuração de rotas com RouterModule.forRoot()
-  exports: [RouterModule]  // Exportação do RouterModule
+  imports: [RouterModule.forRoot(routes)], 
+  exports: [RouterModule]  
 })
 export class AppRoutingModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormComponent } from "../form/form.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
